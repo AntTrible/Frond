f0e73f6c93d7821d34740429723048242d2b5464
AntTrible <13923692100@139.com>
2022-10-08 18:10:27 +0800

    Frond

--- myCloudMusic/pages/songlist/songlist.js
+++ myCloudMusic/pages/songlist/songlist.js
@@ -5,13 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
+     background:{},//轮播图
+     singer:{}//歌手
+  },
 
+
+  //获取banner
+  getbanner:function(){
+    wx.request({
+      url:'http://localhost:3000/banner',
+      dataType:"json",
+
+      success:(result) =>{
+        console.log(result.data.banners)
+        //设置给background
+        this.setData({
+          background:result.data.banners
+        })
+      },
+    })
+  },
+
+
+  //获取歌手
+  getsinger:function(){
+    wx.request({
+      url:'http://localhost:3000/top/artists',
+      dataType:"json",
+
+      success:(result) =>{
+        console.log(result.data.artists)
+        //设置给background
+        this.setData({
+          singer:result.data.artists
+        })
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    
+        this.getbanner()//轮播图
+        this.getsinger()//歌手
 
   },
 
